c99459f37bc9b6b85e470ca0dbc3b4b35923a730
Max Ruttenberg <mrutt@cs.washington.edu>
2023-08-08 09:59:58 -0700

    allocator test changges

--- examples/allocator/allocator.cpp
+++ examples/allocator/allocator.cpp
@@ -14,6 +14,7 @@ DRV_API_REF_CLASS_END(foo)
 
 DrvAPIGlobalDRAM<int> i;
 DrvAPIGlobalDRAM<foo> f;
+DrvAPIGlobalDRAM<DrvAPIPointer<int>> pi;
 
 int AllocatorMain(int argc, char *argv[])
 {
@@ -21,16 +22,19 @@ int AllocatorMain(int argc, char *argv[])
     if (DrvAPIThread::current()->threadId() == 0 &&
         DrvAPIThread::current()->coreId() == 0) {
         DrvAPIMemoryAllocatorInit();
-        DrvAPIPointer<void> p0 = DrvAPIMemoryAlloc(DrvAPIMemoryDRAM, 0x1000);
-        DrvAPIPointer<void> p1 = DrvAPIMemoryAlloc(DrvAPIMemoryDRAM, 0x1000);        
+        DrvAPIPointer<int> p0 = DrvAPIMemoryAlloc(DrvAPIMemoryDRAM, 0x1000);
+        DrvAPIPointer<int> p1 = DrvAPIMemoryAlloc(DrvAPIMemoryDRAM, 0x1000);
         foo_ref fref = &f;
         std::cout << "p0 = 0x" << std::hex << p0 << std::endl;
-        std::cout << "p1 = 0x" << std::hex << p1 << std::endl;        
+        std::cout << "p1 = 0x" << std::hex << p1 << std::endl;
         fref.a() = 1;
         fref.b() = 2;
         std::cout << "&f = 0x" << std::hex << &fref << std::endl;
         std::cout << "f.a = " << fref.a() << std::endl;
-    }    
+        pi[0] = 1;
+        int x = pi[0];
+        std::cout << "pi[0] = " << x << std::endl;
+    }
     return 0;
 }
 declare_drv_api_main(AllocatorMain);
