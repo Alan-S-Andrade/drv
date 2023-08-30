9cfc4bcd17dd65fc0167af335ad27faabb29cc84
Marko <marko.scrbak@amd.com>
2023-08-30 09:15:55 -0700

    Added drv stream benchmark

---
+++ examples/stream/streamtest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DRV_TESTS=$HOME/pando/sst/pando-drv/tests
+numactl --physcpubind=0-95 --membind=0 sst -n 24 --verbose --model-options=" $HOME/pando/sst/pando-drv/examples/stream/stream.so" $DRV_TESTS/drv-multicore-bus-ramu-test.py
+#mpirun -n 8 sst -n 24 --verbose --model-options=" $HOME/pando/sst/pando-drv/examples/stream/stream.so" $DRV_TESTS/drv-multicore-bus-ramu-test.py
