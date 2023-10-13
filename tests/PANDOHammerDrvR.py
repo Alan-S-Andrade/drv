1defd30b975ddad46f296b0b4801b0ba8f0a009c
Max Ruttenberg <mrutt@cs.washington.edu>
2023-10-13 13:06:03 -0700

    [drvr] working script using nic similar to drvx

---
+++ tests/PANDOHammerDrvR.py
@@ -0,0 +1,258 @@
+import sst
+import argparse
+
+SYSCONFIG = {
+    "sys_num_pxn" : 1,
+    "sys_pxn_pods" : 1,
+    "sys_pod_cores" : 8,
+    "sys_core_threads" : 16,
+    "sys_core_clock" : "1GHz",
+}
+
+CORE_DEBUG = {
+    "debug_init" : False,
+    "debug_clock" : False,
+    "debug_requests" : False,
+    "debug_responses" : False,
+    "debug_loopback" : False,
+}
+
+# parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("program", help="program to run")
+parser.add_argument("--verbose", type=int, default=0, help="verbosity of core")
+
+arguments = parser.parse_args()
+
+print("""
+PANDOHammerDrvR:
+  program = {}
+""".format(
+    arguments.program
+))
+class Tile(object):
+    def l1sp_start(self):
+        return (self.id+0)*4*1024
+
+    def l1sp_end(self):
+        return (self.id+1)*4*1024-1
+    
+    def markAsLoader(self):
+        """
+        Set this tile as responsible for loading the executable.
+        """
+        self.core.addParams({"load" : 1})
+
+    def initMem(self):
+        """
+        Initialize the tile's scratchpad memory
+        """
+        # create the scratchpad
+        self.scratchpad_mectrl = sst.Component("scratchpad_mectrl_%d" % self.id, "memHierarchy.MemController")
+        self.scratchpad_mectrl.addParams({
+            "debug" : 0,
+            "debug_level" : 0,
+            "verbose" : 0,
+            "clock" : "1GHz",
+            "addr_range_start" : self.l1sp_start(),
+            "addr_range_end" :   self.l1sp_end(),
+        })
+        # set the backend memory system to Drv special memory
+        # (needed for AMOs)
+        self.scratchpad = self.scratchpad_mectrl.setSubComponent("backend", "Drv.DrvSimpleMemBackend")
+        self.scratchpad.addParams({
+            "verbose_level" : 0,
+            "access_time" : "1ns",
+            "mem_size" : "4KiB",
+        })
+
+        # set the custom command handler
+        # we need to use the Drv custom command handler
+        # to handle our custom commands (AMOs)
+        self.scratchpad_customcmdhandler = self.scratchpad_mectrl.setSubComponent("customCmdHandler", "Drv.DrvCmdMemHandler")
+        self.scratchpad_customcmdhandler.addParams({
+            "verbose_level" : 0,
+        })
+        self.scratchpad_nic = self.scratchpad_mectrl.setSubComponent("cpulink", "memHierarchy.MemNIC")
+        self.scratchpad_nic.addParams({
+            "group" : 1,
+            "network_bw" : "1024GB/s",
+        })
+
+    def initRtr(self):
+        """
+        Initialize the tile's router
+        """
+        # create the tile network
+        self.tile_rtr = sst.Component("tile_rtr_%d" % self.id, "merlin.hr_router")
+        self.tile_rtr.addParams({
+            # semantics parameters
+            "id" : self.id,
+            "num_ports" : 3,
+            "topology" : "merlin.singlerouter",
+            # performance models
+            "xbar_bw" : "1024GB/s",
+            "link_bw" : "1024GB/s",
+            "flit_size" : "8B",
+            "input_buf_size" : "1KB",
+            "output_buf_size" : "1KB",
+        })
+
+        # setup connection rtr <-> core
+        self.tile_rtr.setSubComponent("topology","merlin.singlerouter")
+        self.core_nic_link = sst.Link("core_nic_link_%d" % self.id)
+        self.core_nic_link.connect(
+            (self.core_nic, "port", "1ns"),
+            (self.tile_rtr, "port0", "1ns")
+        )
+
+        # setup connection rtr <-> scratchpad
+        self.scratchpad_nic_link = sst.Link("scratchpad_nic_link_%d" % self.id)
+        self.scratchpad_nic_link.connect(
+            (self.scratchpad_nic, "port", "1ns"),
+            (self.tile_rtr, "port1", "1ns")
+        )
+
+    def initCore(self):
+        """
+        Initialize the tile's core
+        """
+        # create the core
+        self.core = sst.Component("core_%d" % self.id, "Drv.RISCVCore")
+        self.core.addParams({
+            "verbose"   : arguments.verbose,
+            "num_harts" : SYSCONFIG["sys_core_threads"],
+            "clock"     : SYSCONFIG["sys_core_clock"],
+            "program" : arguments.program,
+            #"argv" : ' '.join(argv), @ todo, make this work
+            "core": self.id,
+            "pod" : self.pod,
+            "pxn" : self.pxn,
+        })
+        self.core.addParams(SYSCONFIG)
+        self.core.addParams(CORE_DEBUG)
+        self.core_iface = self.core.setSubComponent("memory", "memHierarchy.standardInterface")
+        self.core_iface.addParams({
+            "verbose" : 0,
+        })
+        self.core_nic = self.core_iface.setSubComponent("memlink", "memHierarchy.MemNIC")
+        self.core_nic.addParams({
+            "group" : 0,
+            "network_bw" : "1024GB/s",
+            "destinations" : "1,2",})
+        self.initSP()
+
+    def initSP(self):
+        # set stack pointers
+        stack_base = self.l1sp_start()
+        stack_bytes = self.l1sp_end() - self.l1sp_start() + 1
+        stack_words = stack_bytes // 8
+        thread_stack_words = stack_words // SYSCONFIG["sys_core_threads"]
+        thread_stack_bytes = thread_stack_words * 8
+        # build a string of stack pointers
+        # to pass a parameter to the core
+        sp_v = ["{} {}".format(i, stack_base + ((i+1)*thread_stack_bytes) - 8) for i in range(SYSCONFIG["sys_core_threads"])]
+        sp_str = "[" + ", ".join(sp_v) + "]"
+        self.core.addParams({"sp" : sp_str})
+
+    def __init__(self, id, pod=0, pxn=0):
+        self.id = id
+        self.pod = pod
+        self.pxn = pxn
+        """
+        Create a tile with the given ID, pod, and PXN.
+        """
+        self.initCore()
+        self.initMem()
+        self.initRtr()
+
+DRAM_BASE = 0x40000000
+DRAM_SIZE = 0x40000000
+
+class SharedMemory(object):
+    def __init__(self, id):
+        self.memctrl = sst.Component("memctrl_%d" % id, "memHierarchy.MemController")
+        self.memctrl.addParams({
+            "clock" : "1GHz",
+            "addr_range_start" : DRAM_BASE+(id+0)*512*1024*1024+0,
+            "addr_range_end"   : DRAM_BASE+(id+1)*512*1024*1024-1,
+            "debug" : 1,
+            "debug_level" : 0,
+            "verbose" : 0,
+        })
+        # set the backend memory system to Drv special memory
+        # (needed for AMOs)
+        self.memory = self.memctrl.setSubComponent("backend", "Drv.DrvSimpleMemBackend")
+        self.memory.addParams({
+            "verbose_level" : 0,
+            "access_time" : "32ns",
+            "mem_size" : "512MiB",
+        })
+        # set the custom command handler
+        # we need to use the Drv custom command handler
+        # to handle our custom commands (AMOs)
+        self.customcmdhandler = self.memctrl.setSubComponent("customCmdHandler", "Drv.DrvCmdMemHandler")
+        self.customcmdhandler.addParams({
+            "verbose_level" : 0,
+        })
+        # network interface
+        self.nic = self.memctrl.setSubComponent("cpulink", "memHierarchy.MemNIC")
+        self.nic.addParams({
+            "group" : 2,
+            "network_bw" : "256GB/s",
+        })
+
+# build the tiles
+tiles = []
+CORES = SYSCONFIG["sys_pod_cores"]
+for i in range(CORES):
+    tiles.append(Tile(i))
+
+tiles[0].markAsLoader()
+
+# build the shared memory
+shared_memory = SharedMemory(0)
+
+# build the network crossbar
+chiprtr = sst.Component("chiprtr", "merlin.hr_router")
+chiprtr.addParams({
+    # semantics parameters
+    "id" : len(tiles),
+    "num_ports" : len(tiles)+1,
+    "topology" : "merlin.singlerouter",
+    # performance models
+    "xbar_bw" : "256GB/s",
+    "link_bw" : "256GB/s",
+    "flit_size" : "8B",
+    "input_buf_size" : "1KB",
+    "output_buf_size" : "1KB",
+})
+chiprtr.setSubComponent("topology","merlin.singlerouter")
+
+# wire up the tiles network
+
+for (i, tile) in enumerate(tiles):
+    bridge = sst.Component("bridge_%d" % i, "merlin.Bridge")
+    bridge.addParams({
+        "translator" : "memHierarchy.MemNetBridge",
+        "debug" : 1,
+        "debug_level" : 10,
+        "network_bw" : "256GB/s",
+    })
+    tile_bridge_link = sst.Link("tile_bridge_link_%d" % i)
+    tile_bridge_link.connect(
+        (bridge, "network0", "1ns"),
+        (tile.tile_rtr, "port2", "1ns")
+    )
+    bridge_chiprtr_link = sst.Link("bridge_chiprtr_link_%d" % i)
+    bridge_chiprtr_link.connect(
+        (bridge, "network1", "1ns"),
+        (chiprtr, "port%d" % i, "1ns")
+    )
+
+# wire up the shared memory
+mem_rtr_link = sst.Link("mem_rtr_link")
+mem_rtr_link.connect(
+    (shared_memory.nic, "port", "1ns"),
+    (chiprtr, "port%d" % len(tiles), "1ns")
+)
