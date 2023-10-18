fa2fa61eefe81ccffc84d79d7a390b4f2a011e69
Max Ruttenberg <mrutt@cs.washington.edu>
2023-10-18 10:38:37 -0700

    [drvx] pedantic fix

--- api/DrvAPIMemory.hpp
+++ api/DrvAPIMemory.hpp
@@ -22,7 +22,7 @@ typedef enum __DrvAPIMemoryType {
 template <typename T>
 T read(DrvAPIAddress address)
 {
-    T result = 0;
+    T result = static_cast<T>(0);
     DrvAPIThread::current()->setState(std::make_shared<DrvAPIMemReadConcrete<T>>(address));
     DrvAPIThread::current()->yield();
     auto read_req = std::dynamic_pointer_cast<DrvAPIMemRead>(DrvAPIThread::current()->getState());
