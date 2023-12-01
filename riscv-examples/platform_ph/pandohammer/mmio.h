97b38ed5c6e4a0422aaac4e9e001366e60703221
mrutt92 <mrutt@cs.washington.edu>
2023-12-01 11:41:39 -0800

    Single-Precision Floating Point (#43)
    * [drvr] floating point registers in hart
    
    * [drvr] fix float register handles
    
    * [drvr] flw/fsw
    
    * [drvr] save
    
    * [drvr] test for floating point load/stores
    
    * [drvr] some fp instructions, only dyn rounding mode for now
    
    * [drvr] missing license in test
    
    * [drvr] fmadd test
    
    * [drvr] save floating point
    
    * [drvr] more floating point instructions
    
    * Multithreaded fmad test
    
    * Changing ph_print_string to ph_puts
    
    * Adding AMD copyright header
    
    * Updating the Makefile with copyright header
    
    ---------
    
    Co-authored-by: Alexandru Dutu <alexandru.dutu@amd.com>

--- riscv-examples/platform_ph/pandohammer/mmio.h
+++ riscv-examples/platform_ph/pandohammer/mmio.h
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2023 University of Washington
+/* Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved. */
 
 #ifndef PANDOHAMMER_MMIO_H
 #define PANDOHAMMER_MMIO_H
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+static inline void ph_print_float(float x)
+{
+    *(volatile float*)0xFFFFFFFFFFFF0000 = x;
+}
 
 static inline void ph_print_int(long x)
 {
@@ -20,6 +26,13 @@ static inline void ph_print_hex(unsigned long x)
 static inline void ph_print_char(char x)
 {
     *(volatile char*)0xFFFFFFFFFFFF0010 = x;
+}
+
+static inline void ph_puts(char *cstr)
+{
+    for (long i = 0; i < strlen(cstr); i++) {
+        ph_print_char(cstr[i]);
+    }
 }
 
 
